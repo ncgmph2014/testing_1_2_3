de5906dd86a5f46961aac03462c29c9f6076db49
ncgmph2014 <nicole.gathany@gmail.com>
2020-02-08 19:50:48 -0500

    finished the problem exponentially more quickly than some of these 6kyus. wow

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 var number = function(array) {
   //your awesome code here
+  let finalArray = [];
+  for (let i = 1; i <= array.length; i++) {
+    finalArray.push(i + ": " + array[i - 1]);
+  }
+
+  return finalArray;
 };
+
+//examples to test
+
+number([]); // => []
+number(["a", "b", "c"]); // => ["1: a", "2: b", "3: c"]
